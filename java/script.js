0998cdf35aaeaee00b71dffe44f2e42be061de43
aya saeed <104939653+ayasaeed13@users.noreply.github.com>
2022-06-17 23:03:11 +0200

    Add files via upload

--- java/script.js
+++ java/script.js
@@ -15,39 +15,40 @@ function onGame(id){
         turn ="x"
         title.innerHTML ="x - Turn"
     }
-    win();
+    winner();
     isDrawn();
 }
 // win case
-function win(){
+let winner = function() {
     for (let i = 1; i < 10; i++) {
-       squares[i]= document.getElementById(`item${i}`).innerHTML; 
-    }
-    
-    if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] !=""){
-        end(1,2,3)
-    }
-    if(squares[4] == squares[5] && squares[5] == squares[6] && squares[4] !=""){
-        end(4,5,6)
-    }
-    if(squares[7] == squares[8] && squares[8] == squares[9] && squares[7] !=""){
-        end(7,8,9)
-    }
-    if(squares[1] == squares[4] && squares[4] == squares[7] && squares[1] !=""){
-        end(1,4,7)
-    }
-    if(squares[2] == squares[5] && squares[5] == squares[8] && squares[2] !=""){
-        end(2,5,8)
-    }
-    if(squares[3] == squares[6] && squares[6] == squares[9] && squares[3] !=""){
-        end(3,6,9)
-    }
-    if(squares[1] == squares[5] && squares[5] == squares[9] && squares[1] !=""){
-        end(1,5,9)
-    }
-    if(squares[3] == squares[5] && squares[5] == squares[7] && squares[3] !=""){
-        end(3,5,7)
-    }
+        squares[i]= document.getElementById(`item${i}`).innerHTML; 
+     }  
+     if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] !=""){
+         end(1,2,3)
+     }
+     if(squares[4] == squares[5] && squares[5] == squares[6] && squares[4] !=""){
+         end(4,5,6)
+     }
+     if(squares[7] == squares[8] && squares[8] == squares[9] && squares[7] !=""){
+         end(7,8,9)
+     }
+     if(squares[1] == squares[4] && squares[4] == squares[7] && squares[1] !=""){
+         end(1,4,7)
+     }
+     if(squares[2] == squares[5] && squares[5] == squares[8] && squares[2] !=""){
+         end(2,5,8)
+     }
+     if(squares[3] == squares[6] && squares[6] == squares[9] && squares[3] !=""){
+         end(3,6,9)
+     }
+     if(squares[1] == squares[5] && squares[5] == squares[9] && squares[1] !=""){
+         end(1,5,9)
+     }
+     if(squares[3] == squares[5] && squares[5] == squares[7] && squares[3] !=""){
+         end(3,5,7)
+     }else{
+        return false;
+     }
 }
 // end game
 function end(num1,num2,num3){
@@ -60,10 +61,9 @@ function end(num1,num2,num3){
 }
 function isDrawn(){
     let filteredSquares = squares.filter((age) => age != "")
-    if(filteredSquares.length == 9){
+    if(filteredSquares.length == 9 && winner == false){
         title.innerHTML=`OOPS !! Game is drawn`
         setInterval(function(){title.innerHTML =" loading new game ... "},1500)
         setTimeout(function(){location.reload()},2000)
     }
 }
-// win and draw call back functions win function comes first then isDrawn function
